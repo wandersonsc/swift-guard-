e166ef8559fa5e6977123e64c7addea480316f61
Wanderson <wandersonsc@gmail.com>
2023-03-06 22:55:40 -0300

    Added new fnc and removed .DS file

--- swiftGuard .playground/Contents.swift
+++ swiftGuard .playground/Contents.swift
@@ -1,3 +1,20 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+
+func doSomething() {
+    guard condition else {
+        // code to execute if condition is false
+        return
+    }
+    // code to execute if condition is true
+}
+
+
+func doSomething() {
+    guard let value = optionalValue else {
+        // code to execute if optionalValue is nil
+        return
+    }
+    // code to execute if optionalValue is not nil, and value is bound to optionalValue
+    print(value)
+}
